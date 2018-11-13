c28b9ea3b4a9ff2a9307043a2638a8a855a88403
gschoe <giseok.choe@gmail.com>
2018-11-13 12:30:46 +0900

    auto matching eval framework

--- auto_matching.py
+++ auto_matching.py
@@ -2,6 +2,16 @@
 import settings
 import requests
 import os
+import json
+
+
+def get_result_path(filepath):
+    outfile = filepath.replace('\\data\\', '\\result\\')
+    obj_outfile = outfile + '.obj_result'
+    outfile = outfile + '.result'
+
+    return outfile, obj_outfile
+
 
 
 def anal(filename, outfile):
@@ -55,9 +65,7 @@ def anal_all():
 
     for filepath in all_filepath:
         if filepath.split('.')[1] == 'jpg':
-            outfile = filepath.replace('\\data\\', '\\result\\')
-            obj_outfile= outfile + '.obj_result'
-            outfile = outfile + '.result'
+            outfile, obj_outfile = get_result_path(filepath)
             dir = os.path.dirname(outfile)
             try:
                 os.makedirs(dir)
@@ -74,3 +82,73 @@ def anal_all():
                 print "skip exist obj result" + obj_outfile
 
 
+def get_person(person_data):
+    name_list = ['monica', 'ross', 'rachel', 'joey', 'chandler', 'phoebe']
+    face_list = list()
+    body_list = list()
+    for name in name_list:
+        if person_data[name][0]['face_rect']['min_x'] != 'none':
+            face = person_data[name][0]['face_rect']
+            face[u'name'] = unicode(name)
+            face_list.append(face)
+        if person_data[name][0]['full_rect']['min_x'] != 'none':
+            body = dict()
+            body[u'object_name'] = u'person'
+            body[u'object_rect'] = person_data[name][0]['full_rect']
+
+            body_list.append(body)
+
+    return face_list, body_list
+
+
+def find_face(face_list):
+    return 1
+
+
+def find_object(obj_list):
+    return 1
+
+
+def eval(answer_file_path):
+    with open(answer_file_path) as data_file:
+        data = json.load(data_file)
+
+    hit_cnt = 0
+    object_cnt = 0
+
+    #print data['visual_results']
+    for image_result in data['visual_results']:
+        print '------------------------'
+        print image_result['image']
+        face_list, body_list = get_person(image_result['person'][0])
+        obj_list = image_result['object']
+        object_cnt = object_cnt + len(face_list)
+        hit_cnt = hit_cnt + find_face(face_list)
+        object_cnt = object_cnt + len(body_list)
+        hit_cnt = hit_cnt + find_object(body_list)
+        object_cnt = object_cnt + len(obj_list)
+        hit_cnt = hit_cnt + find_object(obj_list)
+        print face_list
+        print body_list
+        print obj_list
+
+    return hit_cnt, object_cnt
+
+
+def eval_all():
+    cwd = os.path.realpath(os.path.dirname(__file__))
+    data_wd = os.path.join(cwd, 'data', 'S01_EP11_23_Visual_final')
+
+    all_hit_cnt = 0
+    all_obj_cnt = 0
+
+    all_filepath = getListOfFiles(data_wd)
+    for filepath in all_filepath:
+        if filepath.split('.')[1] == 'json':
+            hit_cnt, obj_cnt = eval(filepath)
+            all_hit_cnt = all_hit_cnt + hit_cnt
+            all_obj_cnt = all_obj_cnt + obj_cnt
+
+    print
+    print "(hit) {} / {} (all)".format(all_hit_cnt, all_obj_cnt)
+    print "Auto Matching Success Rate : {}".format(float(all_hit_cnt)/float(all_obj_cnt))
--- eval.py
+++ eval.py
@@ -1,3 +1,4 @@
 
 import auto_matching
 
+auto_matching.eval_all()
